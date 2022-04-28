f3b9f922416248696417875f96ab617c3c3a2fe4
Tanner Vasica <tmanvas06@hotmail.com>
2022-04-28 14:18:32 -0600

    final

--- server/index.js
+++ server/index.js
@@ -14,11 +14,35 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+
+
 app.get(`/`, (req,res) => {
     res.sendFile(path.join(__dirname, `../index.html`))
     rollbar.info('html file served successfully.')
 })
 
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
 const port = process.env.PORT || 4545
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`listening on port ${port}`))
